3844cc64a76860490d8f6ea28ad556e39792c38b
Alyssa Derensy <122968873+AlyssaDerensy@users.noreply.github.com>
2023-05-24 12:00:45 +0200

    Update requete_3.sql

--- requete_3.sql
+++ requete_3.sql
@@ -1,8 +1,6 @@
+Proportion des ventes d’appartements par le nombre de pièces 
+
 Select Total_piece, round(count(Total_piece)*100/(Select count(Total_piece) from bien),2) as "Pourcentage"
 from bien
 group by Total_piece	
 Order by Total_piece
-
-
-
-
