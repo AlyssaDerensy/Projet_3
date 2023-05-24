852c6c0b9e14be344407afcfcc90fb42008d3426
Alyssa Derensy <122968873+AlyssaDerensy@users.noreply.github.com>
2023-05-24 12:03:33 +0200

    Update requete_11.sql

--- requete_11.sql
+++ requete_11.sql
@@ -1,3 +1,5 @@
+Les moyennes de valeurs foncières pour le top 3 des communes des départements 6, 13, 33, 59 et 69 
+
 With t1 as(
 Select Code_departement as Departement, Nom_commune as Commune, round(AVG(Valeur_fonciere),2) as Moyenne_valeur from vente
 join commune using(Id_codedep_codecommune)
@@ -8,4 +10,3 @@ Select Commune,Departement, Moyenne_valeur,
 rank() over(partition by Departement order by Moyenne_valeur desc) as rang from t1
 Order by rang
 limit 15
-
